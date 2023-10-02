5ad04903be874c056ab365a6cf6652adc650bbb8
Your Name <your_netid@cornell.edu>
2023-10-02 00:55:01 -0400

    initial training loop

---
+++ dataset.py
@@ -0,0 +1,16 @@
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+
+class PeptideDataset(Dataset):
+    def __init__(self):
+        xy = np.loadtxt('./data_parsed/data.csv', delimiter=",", dtype=np.float32)
+        self.x = torch.from_numpy(xy[:, :12])
+        self.y = torch.from_numpy(xy[:, [-1]])
+        self.n_samples = xy.shape[0]
+
+    def __getitem__(self, index):
+        return self.x[index], self.y[index]
+
+    def __len__(self):
+        return self.n_samples
