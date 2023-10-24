6914b65dff0c6951b2ef7dccd15bd843156f4370
Your Name <your_netid@cornell.edu>
2023-10-24 14:15:39 -0400

    Toy data training loop

--- PeptideDataset.py
+++ PeptideDataset.py
@@ -14,3 +14,15 @@ class PeptideDataset(Dataset):
 
     def __len__(self):
         return self.n_samples
+class ToyPeptideDataset(Dataset):
+    def __init__(self):
+        xy = np.loadtxt('./data_parsed/6AA_data_neg_norm.csv', delimiter=",", dtype=np.float32)
+        self.x = torch.from_numpy(xy[:, :6])
+        self.y = torch.from_numpy(xy[:, [-1]])
+        self.n_samples = xy.shape[0]
+
+    def __getitem__(self, index):
+        return self.x[index], self.y[index]
+
+    def __len__(self):
+        return self.n_samples
